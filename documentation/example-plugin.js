1c2b25f4b420b86aa655260a4090037e009ea05f
OFRBG <hiro@cehh.io>
2019-04-11 22:33:43 -0500

    Tidy things up

---
+++ documentation/example-plugin.js
@@ -0,0 +1,27 @@
+window.$docsify.plugins = (window.$docsify.plugins||[]).concat([
+  function(hook, vm) {
+    var tagRegex = /<codesandbox.*>(\s+)?([^\s]*)(\s+)?<\/codesandbox>/g;
+    var cleanup = function(html) {
+      return html.replace(tagRegex, '<codesandbox example="$2" class="javascript"></codesandbox>');
+    }
+    var embed = function(html) {
+      var iframe = (
+        `<codesandbox>$2</codesandbox><div style="display: flex; justify-content: center;">
+          <button class="btn btn-web3js">Load CodeSandbox</button>
+          <iframe
+            data-src="https://codesandbox.io/embed/github/${config.orgName}/${config.repoName}/tree/${config.branch}/example-src/packages/$2?fontsize=12&hidenavigation=1&view=editor&codemirror=1&runonclick=1&editorsize=60"
+            style="margin:0px; display:none; width:100%; height:350px; border:2px; border-radius:5px; overflow:hidden;"
+            sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+          ></iframe>
+        </div>`
+      );
+      return html.replace(tagRegex, iframe);
+    }
+    hook.beforeEach(function(content) {
+      return content.replace(tagRegex, `#### Example\n<codesandbox>$2</codesandbox>`)
+    });
+    hook.afterEach(function(html, next) {
+      next(cleanup(embed(html)));
+    });
+  },
+]);
