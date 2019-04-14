a0fd8a9b84bedee91cb24cee57e0026bf7291b98
OFRBG <hiro@cehh.io>
2019-04-14 15:15:23 -0500

    Open console window by default and display vertically

--- documentation/example-plugin.js
+++ documentation/example-plugin.js
@@ -9,8 +9,8 @@ window.$docsify.plugins = (window.$docsify.plugins||[]).concat([
         `<codesandbox>$2</codesandbox><div style="display: flex; justify-content: center;">
           <button class="btn btn-web3js">Load CodeSandbox</button>
           <iframe
-            data-src="https://codesandbox.io/embed/github/${config.orgName}/${config.repoName}/tree/${config.branch}/example-src/packages/$2?fontsize=12&hidenavigation=1&view=editor&codemirror=1&runonclick=1&editorsize=60"
-            style="margin:0px; display:none; width:100%; height:350px; border:2px; border-radius:5px; overflow:hidden;"
+            data-src="https://codesandbox.io/embed/github/${config.orgName}/${config.repoName}/tree/${config.branch}/example-src/packages/$2?fontsize=12&hidenavigation=1&view=editor&codemirror=1&runonclick=1&previewwindow=console"
+            style="margin:0px; display:none; width:100%; height:600px; border:2px; border-radius:5px; overflow:hidden;"
             sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
           ></iframe>
         </div>`
